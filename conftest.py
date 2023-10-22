07b2dcbf3b6c753e40a8388ab1f3ecbaa039db7d
osteron <dimaslepyan@gmail.com>
2023-10-22 22:01:39 +0300

    Рефактор conftest и requirements

--- conftest.py
+++ conftest.py
@@ -19,17 +19,13 @@ def url(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", default="chrome", help="Browser to run tests")
     parser.addoption("--url", default="http://localhost:8081", help="Opencart URL")
-    parser.addoption("--executor", default="127.0.0.1")
-    parser.addoption("--drivers", default="/chromedriver", help="Drivers path")
 
 
 @pytest.fixture
 def browser(request):
     browser = request.config.getoption("--browser")
-    driver = request.config.getoption("--drivers")
-    executor = request.config.getoption("--executor")
     url = request.config.getoption("--url")
-    executor_url = f"http://{executor}:4444/wd/hub"
+
     if browser == "chrome":
         driver = webdriver.Chrome(service=ChromeService())
     elif browser == "firefox":
