a0c870bc8b363a4db7b5744051bc426430822904
osteron <dimaslepyan@gmail.com>
2023-10-23 11:46:52 +0300

    Рефакторинг conftest.py

--- conftest.py
+++ conftest.py
@@ -35,7 +35,9 @@ def browser(request):
     if executor == "local":
         if browser == "chrome":
             options = webdriver.ChromeOptions()
-            options.headless = True
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-gpu")
             driver = webdriver.Chrome(options=options)
         elif browser == "firefox":
             driver = webdriver.Firefox()
